c0ea81a4594857ee305f77df8a120ead0088c723
glinka <alexandraglinka@yandex.ru>
2023-07-02 16:38:36 +0300

    initial coomit

---
+++ src/test/java/pro/sky/java/course2/calculator_tests/CalculatorTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.calculator_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
